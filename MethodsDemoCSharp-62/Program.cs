535ffea6b6f35836bdf48dcb11f8f9175f4d51f4
SethBowman <brandonsethbowman@gmail.com>
2025-06-16 10:34:53 -0500

    demo completed

---
+++ MethodsDemoCSharp-62/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace MethodsDemoCSharp_62;
+
+class Program
+{
+    //Create custom methods here
+    
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //string response = Console.ReadLine();
+        
+        //Calling the method (using a method)
+        //Console.ForegroundColor = ConsoleColor.Green;
+        GreetUserByName("Seth");
+        GreetUserByName("Cynthia");
+
+        double sum = DoubleAddition(2.5, 2.5);
+        Console.WriteLine($"Sum = {sum}");
+    }
+    
+    //Or create custom methods here
+    
+    //Method definition (creating a method)
+    //access modifier - return type - method name (PascalCase) - parameter list - scope/body
+    public static void GreetUserByName(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
+
+    public static double DoubleAddition(double numOne, double numTwo)
+    {
+        //double sum = numOne + numTwo;
+
+        return numOne + numTwo;
+    }
+}
